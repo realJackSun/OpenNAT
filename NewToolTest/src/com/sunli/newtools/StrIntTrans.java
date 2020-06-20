8df9954fe90f61909296214ea1ecac8d6a4be2b8
JackSun <sjtusl@163.com>
2020-06-20 19:18:59 +0800

    web service part

---
+++ NewToolTest/src/com/sunli/newtools/StrIntTrans.java
@@ -0,0 +1,11 @@
+package com.sunli.newtools;
+
+public class StrIntTrans {
+	public static void main(String [] args){
+		int a = 1;
+		System.out.println(a+"");//int to str
+		String b = "123";
+		System.out.println(Integer.valueOf(b));
+		System.out.println("asdf"+1+"234");
+	}
+}
